fccf92ca131eb5a3c52ac2c3e8b5ad0ed1011804
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-12-26 10:43:14 -0500

    3.Autenticación y Autorización

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,9 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import authRoutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,6 +38,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/*ROUTES WITH FILES*/
+app.post('/auth/register', upload.single('picture'), register);
+
+/*ROUTES*/
+app.use('/auth', authRoutes);
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;

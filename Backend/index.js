bca3df497ee30c1d180aedb19f53ddc6e05b4f5b
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-12-30 15:13:12 -0500

    5.Post Routes

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,12 @@ import { fileURLToPath } from 'url';
 
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/postRoutes.js';
+
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/postController.js';
+
+import { verifyToken } from './middleware/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -42,10 +47,12 @@ const upload = multer({ storage });
 
 /*ROUTES WITH FILES*/
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 /*ROUTES*/
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 /*MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;

3c51277f1733138385428b7534dcfa1ace074506
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-01-01 18:43:56 -0500

    8.Folder Architecture and React Router y 9.Configurando State

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+
+import HomePage from 'scenes/homePage/indexHomePage';
+import LoginPage from 'scenes/loginPage/indexLoginPage';
+import ProfilePage from 'scenes/profilePage/indexProfile';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

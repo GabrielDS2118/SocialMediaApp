10c303a90502fb5d27629c4a5d08b4a528140338
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-01-06 12:41:09 -0500

    14.Home Page: Crear Post

--- frontend/src/scenes/homePage/indexHomePage.jsx
+++ frontend/src/scenes/homePage/indexHomePage.jsx
@@ -2,7 +2,7 @@ import { Box, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Navbar from 'scenes/navbar/indexNv';
 import UserWidget from 'scenes/widgets/UserWidget';
-
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
   const { _id, picturePath } = useSelector((state) => state.user);
@@ -25,7 +25,9 @@ const HomePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? '42%' : undefined}
           mt={isNonMobileScreens ? undefined : '2rem'}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath}></MyPostWidget>
+        </Box>
 
         {isNonMobileScreens && <Box flexBasis="26%"></Box>}
       </Box>
